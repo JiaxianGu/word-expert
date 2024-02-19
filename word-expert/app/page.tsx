fa70108f75bf745e9c809a98a7ef4d9e03ef827c
JiaxianGu <gujiaxian1997@gmail.com>
2024-02-19 13:49:17 +0900

    update tab page title

--- word-expert/app/page.tsx
+++ word-expert/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main><h1>Hello world test deployment</h1></main>
+    <main><h1>Hello world test deployment yes!!!</h1></main>
   );
 }
