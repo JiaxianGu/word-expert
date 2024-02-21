9e0dd472bdb77a0a91383fc6dd1b4f8e616dc8ee
JiaxianGu <gujiaxian1997@gmail.com>
2024-02-22 00:35:08 +0900

    feat: implement My words UI and logic

--- word-expert/app/components/Library.jsx
+++ word-expert/app/components/Library.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
+let BACKENDURL = ''
 
-const Library = () => {
 
+const Library = ({ userName }) => {
+  const [words, setWords] = useState([])
+  if (process.env.NODE_ENV === 'development') {
+    BACKENDURL = 'http://localhost:8080';
+  } else if (process.env.NODE_ENV === 'production') {
+    BACKENDURL = 'https://word-expert-backend.onrender.com';
+  }
+  const fetchWords = async () => {
+    try {
+      const reqBody = JSON.stringify({ "user": userName });
+      console.log(reqBody);
+      const response = await fetch(`${BACKENDURL}/get`, 
+      {method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: reqBody,});
+      const data = await response.json();
+      setWords(data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchWords();
+  }, []);
 
   return (
-    <div className="h-screen flex flex-wrap items-center justify-center ">
-      <p className='w-full border-2 border-black'>this1</p>
-      <p className='border-2 border-black'>this2</p>
-
+    <div className="max-w-4xl mx-auto p-8">
+      <h1 className="text-2xl font-bold text-center mb-6">Learned Words</h1>
+      <div className="divide-y divide-gray-300">
+        
+        {words.map(({ word, meaning }, index) => (
+          <div key={index} className="py-4 flex flex-col md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-4">
+            <span className="text-lg font-medium w-80">{word}</span>
+            <span className="text-gray-600">{meaning}</span>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
